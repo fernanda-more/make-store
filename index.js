9e5440f8e2227c0ebdd7058a971c6efb8db31761
fernanda-more <nanda.mooh@gmail.com>
2025-08-22 14:42:52 -0300

    adiciona multiplicacao

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-console.log("Oi")
-console.log("Test")
+console.log("Calculadora")
 
 const a = 24;
 const b = 2;
@@ -7,3 +6,4 @@ const b = 2;
 console.log(`Soma = ${a+b}`)        
 console.log(`Substracao = ${a-b}`)
 console.log(`Divisao = ${a/b}`)
+console.log(`Multiplicacao = ${a*b}`)
