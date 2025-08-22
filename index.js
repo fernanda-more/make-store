206feda3f9d0f5b8a8bea3a2e780693051eea7ec
fernanda-more <nanda.mooh@gmail.com>
2025-08-22 14:39:50 -0300

    adiciona divisao

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 console.log("Oi")
 console.log("Test")
 
-const a = 23;
+const a = 24;
 const b = 2;
 
-console.log(`Soma = ${a+b}`)
-
+console.log(`Soma = ${a+b}`)        
 console.log(`Substracao = ${a-b}`)
+console.log(`Divisao = ${a/b}`)
