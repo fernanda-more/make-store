840e68a895de3789401034518eea223c9aa8a2eb
Fernanda Moreira <nanda.mooh@gmail.com>
2025-09-05 19:18:24 -0300

    feat(test): test

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-console.log("Calculadora")
-
-const a = 24;
-const b = 2;
-
-console.log(`Soma = ${a+b}`)        
-console.log(`Substracao = ${a-b}`)
-console.log(`Divisao = ${a/b}`)
-console.log(`Multiplicacao = ${a*b}`)
+console.log("Hello World")
